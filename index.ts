e629b8a1742d5477bf0feb8cb01e92b263306d4f
Fabrizio Rodriguez <fabriziora12@gmail.com>
2023-01-21 11:26:36 -0500

    nuevas rutas , migracion de las tablas

--- index.ts
+++ index.ts
@@ -27,3 +27,44 @@ app.get('/home', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`El servidor se ejecuta en http://localhost:${port}`);
   });
+
+app.get('/api/v1/users', async (req: Request, res: Response) => {
+const users = await prisma.user.findMany({
+    select: {
+        id: true,
+        name: true,
+        email: true,
+        last_session: true,
+        created_at: true,
+        date_born: true
+    }
+});
+res.json(users);
+});
+  
+app.post('/api/v1/songs', async (req: Request, res: Response) => {
+    const songs = await prisma.song.findMany();
+    res.json(songs);
+});
+
+  
+app.post("/api/v1/playlists", async (req, res) => {
+try {
+    const newPlaylist = await prisma.playlist.create({
+    data: {
+        name: req.body.name,
+        user_id: req.body.user_id,
+        songs: {
+        connect: req.body.songs.map((song: any) => ({ id: song.id }))
+        }
+    }
+    });
+    res.json(newPlaylist);
+} catch (error) {
+    res.status(500).json({
+        success: false,
+        message: 'Error al obtener las playlist',
+        
+    });
+}
+});
