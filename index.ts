ea64292415985613a71ed44bbf933c599605741c
Fabrizio Rodriguez <fabriziora12@gmail.com>
2023-01-22 22:49:06 -0500

    add 3 ultimas rutas y modificar tura home por solo / 2.0

--- index.ts
+++ index.ts
@@ -19,14 +19,12 @@ const port = process.env.PORT;
 app.use(express.json());
 
 //Home de prueba
-app.get('/home', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hola, Welcome')
 });
 
 
-app.listen(port, () => {
-    console.log(`El servidor se ejecuta en http://localhost:${port}`);
-  });
+
 
 app.post('/api/v1/users', async (req: Request, res: Response) => {
 const user = req.body as { name: string, email: string, password: string, date_born: string};
@@ -49,7 +47,7 @@ res.json(newUser);
 });
 
 
-app.get('/api/v1/users', async (req: Request, res: Response) => {
+app.get('/api/v1/getusers', async (req: Request, res: Response) => {
 const users = await prisma.user.findMany({
     select: {
         id: true,
@@ -68,6 +66,63 @@ app.post('/api/v1/songs', async (req: Request, res: Response) => {
     res.json(songs);
 });
 
+app.get("/api/v1/getsongs", async (req, res) => {
+    try {
+        const songs = await prisma.song.findMany();
+        const songsWithPlaylist = await Promise.all(songs.map(async (song) => {
+            const playlists = await prisma.playlist.findMany({
+                where: {
+                    songs: {
+                        some: {
+                            id:song.id
+                        }
+                    }
+                },
+                select: {
+                    name: true
+                }
+            });
+            return {
+                ...song,
+                playlists
+            }
+        }))
+        res.json({songsWithPlaylist});
+    } catch (error) {
+    res.status(500).json({
+        success: false,
+        message: 'Error al obtener las canciones',
+        
+    });
+  }
+  });
+
+app.get("/api/v1/getsongs/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const songs = await prisma.song.findMany({ where: { id: Number(id) } });
+      const song = songs[0];
+        if (!song) {
+            return res.status(404).json({ error: 'song not found' });
+        }
+        const playlists = await prisma.playlist.findMany({
+            where: {
+                songs: {
+                    some: {
+                        id: Number(id)
+                    }
+                }
+            }
+        });
+        res.json({song, playlists});
+    } catch (error) {
+      res.status(500).json({
+          success: false,
+          message: 'Error al obtener la cancion',
+          
+      });
+    }
+  });
   
 app.post("/api/v1/playlists", async (req, res) => {
 try {
@@ -89,3 +144,54 @@ try {
     });
 }
 });
+
+app.get("/api/v1/getplaylists", async (req, res) => {
+    try {
+      const playlists = await prisma.playlist.findMany({
+        include: { songs: true }
+      });
+      res.json(playlists);
+    } catch (error) {
+      res.status(500).json({
+          success: false,
+          message: 'Error al obtener las playlists',
+          
+      });
+    }
+  });
+
+app.post("/api/v1/playlists/addsong", async (req, res) => {
+const { id_song, id_playlist } = req.body;
+try {
+    const songs = await prisma.song.findMany({ where: { id: id_song } });
+    const song = songs[0];
+    const playlists = await prisma.playlist.findMany({ where: { id: id_playlist } });
+    const playlist = playlists[0];
+    if (!song) {
+        return res.status(404).json({ error: 'song not found' });
+    }
+    if (!playlist) {
+        return res.status(404).json({ error: 'playlist not found' });
+    }
+    await prisma.playlist.update({
+        where: { id: id_playlist },
+        data: {
+            songs: {
+                connect: { id: id_song }
+            }
+        }
+    });
+    res.json({ message: "song added successfully to the playlist" });
+} catch (error) {
+    res.status(500).json({
+        success: false,
+        message: 'Error al añadir la cancion',
+        
+    });
+}
+});
+  
+
+app.listen(port, () => {
+    console.log(`El servidor se ejecuta en http://localhost:${port}`);
+  });
